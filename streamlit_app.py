aa0e48d8a188f1f842702af6343f1924d8e49257
Hiking427 <35043434+Hiking427@users.noreply.github.com>
2022-11-20 12:09:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥣 Omiga 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-boiled Free-Range Egg")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
