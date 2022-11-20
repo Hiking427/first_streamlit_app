67ae3f7aef0fecf49de31702c3325f039d628ce9
Hiking427 <35043434+Hiking427@users.noreply.github.com>
2022-11-20 12:08:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header("Breakfast Favorites")
-streamlit.text("Omiga 3 & Blueberry Oatmeal")
+streamlit.text("🥣 Omiga 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-boiled Free-Range Egg")
